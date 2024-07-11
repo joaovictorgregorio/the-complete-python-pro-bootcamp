a91f978edd81b0528febed3de79ade55b5052f0c
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-07-11 10:39:20 -0300

    Desafio hangman finalizado com sucesso, todas funcionalidades propostas nas aulas em funcionamento

--- section-7/Challenge-Hangman/main.py
+++ section-7/Challenge-Hangman/main.py
@@ -2,73 +2,17 @@ import random
 from time import sleep
 import os
 import sys
-
-def close_application():
-    sys.exit()
+from hangman_words import word_list
+import hangman_art
 
 os.system('cls')
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
-word_list = ["porcodaterra", "babuino", "camelo"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 lives = 6
-print(f"A palavra selecionada pelo sistema foi: {chosen_word}")
+
+print(hangman_art.logo)
+print(f"\nA palavra selecionada pelo sistema foi: {chosen_word}")
 
 display = []
 for _ in range(word_length):
@@ -77,23 +21,26 @@ for _ in range(word_length):
 while "_" in display:
     guess = input("Adivinhe uma letra: ").lower()
 
+    os.system('cls')
+
     if guess in display:
-        print("Você já adivinhou essa letra. Tente novamente.")
+        print("\033[33mVocê já adivinhou essa letra. Tente novamente.\033[m")
 
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-        if guess not in chosen_word:
-            lives -= 1
-            print(stages[lives])
-            guess = input("Adivinhe uma letra: ").lower()
-            if lives == 0:
-                print("\033[31mVocê perdeu!")
-                close_application()
+    if guess not in chosen_word:
+        print(f"A letra \033[1;36m{guess.upper()}\033[m não está na palavra. \033[1;31mVocê perdeu uma vida.\033[m")
+        lives -= 1
+        if lives == 0:
+            print("\033[1;31mVocê perdeu!")
+            break
 
     print(f"{' '.join(display)}")
 
     if "_" not in display:
-        print("\033[32mVocê venceu!")
+        print("\033[1;32mVocê venceu!")
         break
+
+    print(hangman_art.stages[lives])
