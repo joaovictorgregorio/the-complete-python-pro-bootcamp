2a2d850fb355aa8310efa353d84882e0e0ebbd29
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-09-25 13:37:52 -0300

    Seção 20 finalizada com sucesso, desenvolvido a primeira parte do jogo, onde a cobra se move corretamente como esperado e segue o caminho conforme os comandos enviados pelo teclado

--- section 01 to section 20/section-20/snake_game/main.py
+++ section 01 to section 20/section-20/snake_game/main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 from os import system
 from time import sleep
 
@@ -10,31 +11,19 @@ screen.bgcolor("black")  # Cor de fundo
 screen.title("Snake Game")  # Título da janela
 screen.tracer(0)
 
-segments = []
+snake = Snake()  # Instânciamento da classe Snake
 
-for square in range(3):
-    new_segment = Turtle(shape="square")  # Cria um novo quadrante
-    new_segment.color("white")  # Cor do quadrante
-    new_segment.penup()  # Remove o tracejado da linha quando se move
-    new_segment.goto(x=-20 * square, y=0)  # Adiciona novos quadrantes a tela
-    segments.append(new_segment)
+screen.listen()  # Escuta os eventos do teclado
+screen.onkey(snake.up, "Up")  # Tecla para cima
+screen.onkey(snake.down, "Down")  # Tecla para baixo
+screen.onkey(snake.left, "Left")  # Tecla para esquerda
+screen.onkey(snake.right, "Right")  # Tecla para direita
 
 game_is_on = True
 while game_is_on:
-    screen.update()  # Atualiza a tela (necessário para mostrar os quadrantes
+    screen.update()  # Atualiza a tela (necessário para mostrar os quadrantes)
     sleep(0.1)  # Define um tempo para deslocar, biblioteca time
-
-    """Código que mantém a movimentaçõa da cobra agrupada,
-    sem separar os quadrantes"""
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
-
-
-
+    snake.move()  # Executa a movimentação dos quadrantes da classe Snake
 
 
 screen.exitonclick()  # Mantém a janela aberta até que seja clicado
---
+++ section 01 to section 20/section-20/snake_game/snake.py
@@ -0,0 +1,73 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake():
+    """
+    Código que cria a cobra, adiciona os quadrantes e mantém a movimentação da
+    cobra agrupada em um só lugar para facilitar o código do main.py e do
+    snake_game.py
+    """
+    def __init__(self):
+        """
+        Inicializa a cobra com três quadrantes na tela
+        """
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        """
+        Cria a cobra com três quadrantes iniciais na tela
+        """
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")  # Cria um novo quadrante
+            new_segment.color("white")  # Cor do quadrante
+            new_segment.penup()  # Remove o tracejado da linha quando se move
+            new_segment.goto(position)  # Adiciona novos quadrantes a tela
+            self.segments.append(new_segment)
+
+    def move(self):
+        """
+        Movimenta a cobra, adicionando um novo quadrante na frente e removendo
+        o último quadrante.
+        """
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """
+        Movimenta a cobra para cima
+        """
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """
+        Movimenta a cobra para baixo
+        """
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """
+        Movimenta a cobra para esquerda
+        """
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """
+        Movimenta a cobra para direita
+        """
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
